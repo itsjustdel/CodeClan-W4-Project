1268a52013237eb2737df6660b76dd1ce0a7d400
Derek Reid <itsjustdel@gmail.com>
2021-12-04 18:14:30 +0000

    Show victor

--- controllers/game_controller.py
+++ controllers/game_controller.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, Blueprint
+from models.game import Game
 import repositories.game_repository as game_repository
 import repositories.team_repository as team_repository
 import repositories.league_repository as league_repository
@@ -27,8 +28,28 @@ def show_league_games(league_id):
     route = "leagues"
     return render_template("games/show.html",route=route, object=league, games=games)
 
-# @games_blueprint.route("leagues/<league_id>/games/new")
-# def new_game():
-#     # to create a new game we need
-#     # a list of all teams in the league
-#     teams = 
+@games_blueprint.route("/leagues/<league_id>/games/new")
+def new_game(league_id):
+    # to create a new game we need
+    # a list of all teams in the league
+    league = league_repository.select(league_id)
+    teams = team_repository.teams(league)
+    
+    return render_template("games/new.html", teams=teams, league=league)
+
+
+@games_blueprint.route("/leagues/<league_id>/games/new", methods=['POST'])
+def play_game(league_id):
+    # get league
+    league = league_repository.select(league_id)    
+    # retreive info from form
+    home_team = team_repository.select(request.form['home_team_id'])
+    away_team = team_repository.select(request.form['away_team_id'])    
+    # create a Game object
+    game = Game(league,home_team,away_team)
+    # play the game and find out who won!
+    game.play()
+    # save to db
+    game_repository.save(game)
+    
+    return render_template("games/result.html", game=game)
--- models/game.py
+++ models/game.py
@@ -1,12 +1,12 @@
 from models.team import Team
 class Game:    
-    def __init__(self, league, home_team, away_team, draw, winning_team = None):        
+    def __init__(self, league, home_team, away_team, draw = None, winning_team = None):        
         self.league = league
         self.home_team = home_team
         self.away_team = away_team        
         self.draw = draw
         self.winning_team = winning_team
 
-def play_game(home_team, away_team):
-    # game logic!
-    pass
+    def play(self):
+        self.winning_team = self.home_team # GAME LOGIC TO GO HERE TODO
+        self.draw = False
