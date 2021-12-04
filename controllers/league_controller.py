421629ee1643e5aea23fc91f6b8e1b58fef6e2d2
Derek Reid <itsjustdel@gmail.com>
2021-12-04 11:22:58 +0000

    Add League

--- controllers/league_controller.py
+++ controllers/league_controller.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, Blueprint
+from models.league import League
 import repositories.league_repository as league_repository
 import repositories.team_repository as team_repository
 
@@ -14,3 +15,15 @@ def show(id):
     league = league_repository.select(id)
     teams = team_repository.teams(league)
     return render_template("leagues/show.html",league=league, teams=teams)
+
+
+@leagues_blueprint.route("/leagues/new")
+def new_league_form():
+    return render_template("/leagues/new.html")
+
+@leagues_blueprint.route("/leagues/new", methods=['POST'])
+def new_league():        
+    league_name = request.form['league_name']
+    league = League(league_name)
+    league_repository.save(league)    
+    return leagues()
