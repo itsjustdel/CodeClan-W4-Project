71f222b9d730b44f73b6ccb72d8052c856a07dbf
Derek Reid <itsjustdel@gmail.com>
2021-12-07 20:30:13 +0000

    Team tests

--- run_tests.py
+++ run_tests.py
@@ -2,7 +2,7 @@ import unittest
 from tests.game_test import TestGame
 from tests.league_test import TestLeague
 from tests.monster_test import TestMonster
-# from tests.team_test import TestTeam
+from tests.team_test import TestTeam
 
 if __name__ == '__main__':
     unittest.main()
--- tests/team_test.py
+++ tests/team_test.py
@@ -3,3 +3,11 @@ from models.team import Team
 from models.league import League
 class TestTeam(unittest.TestCase):
     def setUp(self):
+        self.league_1 = League("Test League")
+        self.team_1 = Team("Test Team",self.league_1)
+
+    def test_team_name__Test_Team(self):
+        self.assertEqual("Test Team" , self.team_1.name)
+
+    def test_team_league__league_1(self):
+        self.assertEqual(self.team_1.league, self.league_1)
