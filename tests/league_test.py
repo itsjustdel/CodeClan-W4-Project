5b88e88ad134687a0f70de61f4abee871fcb8b70
Derek Reid <itsjustdel@gmail.com>
2021-12-07 21:34:34 +0000

    sort by teams test

--- tests/league_test.py
+++ tests/league_test.py
@@ -13,18 +13,19 @@ class TestLeague(unittest.TestCase):
 
     def test_sort_teams_by_wins(self):
 
-        team_1 = Team("Test Team 1",self.league_1)
-        team_2 = Team("Test Team 2",self.league_1)
-        game_1 = Game(self.league_1,team_1, team_2)        
-        monster_1 = Monster("John",1,"Blizzard",team_1)
+        team_1 = Team("Test Team 1",self.league_1,0)
+        team_2 = Team("Test Team 2",self.league_1,1)
+            
+        monster_1 = Monster("John",4,"Blizzard",team_1)
         monster_2 = Monster("John",4,"Blizzard",team_1)
         monster_3 = Monster("John",4,"Blizzard",team_1)
         monster_4 = Monster("John",4,"Blizzard",team_2)
         monster_5 = Monster("John",4,"Blizzard",team_2)
-        monster_6 = Monster("John",8,"Blizzard",team_2)
+        monster_6 = Monster("John",20,"Blizzard",team_2)
         monsters_1 = [monster_1,monster_2,monster_3]
         monsters_2 = [monster_4,monster_5,monster_6]     
         
+        game_1 = Game(self.league_1,team_1, team_2)    
         weather = "Blizzard"
         game_1.play(monsters_1,monsters_2, weather)
         
@@ -32,10 +33,13 @@ class TestLeague(unittest.TestCase):
         games = [game_1]
         teams_and_wins = sort_teams_by_wins(teams,games)
 
-        # team 1 will be first in list return because they won the match
-        # the first John in team 1 wins all the time because he has more limbs
-        expected = "Test Team 1"
-        # first list is list of Team and wins tuples, second is Team object itself
+        # team 2 will be first in list return because they won the match
+        # the last John in team 2 wins all the time because he has more limbs
+        expected = "Test Team 2"
+        # first list is list of tuples, second is the team object
         actual = teams_and_wins[0][0].name
+        self.assertEqual(actual, expected)
 
+        expected = "Test Team 1"        
+        actual = teams_and_wins[1][0].name
         self.assertEqual(actual, expected)
